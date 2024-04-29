8d6cb77d3a30d5e5086e3cd557f88ddf72d9644d
ajaxStardust <94220040+ajaxStardust@users.noreply.github.com>
2024-04-29 04:52:22 -0400

    for the turnstiles
    neil young song

--- config/app.php
+++ config/app.php
@@ -39,7 +39,7 @@ return [
     |
     */
 
-    'debug' => (bool) env('APP_DEBUG', false),
+    'debug' => (bool) env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://wsldebian/refbook'),
 
     'asset_url' => env('ASSET_URL', null),
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -17,7 +18,9 @@
     @yield('beforecss')
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     @yield('aftercss')
+    <link rel="icon" href="{{ url('/') }}/favicon.ico">
 </head>
+
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
@@ -25,7 +28,9 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'RefBook') }}
                 </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                    aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -39,43 +44,44 @@
                     <ul class="navbar-nav ml-auto">
                         <!-- Authentication Links -->
                         @foreach($categories as $category)
-                            <li class="nav-item d-block d-lg-none">
-                                <a href="{{ url($category->slug) }}" class="nav-link border-bottom">   {{-- if active bg-muted --}}
-                                    {{ $category->name }}
-                                </a>
-                            </li>
+                        <li class="nav-item d-block d-lg-none">
+                            <a href="{{ url($category->slug) }}" class="nav-link border-bottom">
+                                {{-- if active bg-muted --}}
+                                {{ $category->name }}
+                            </a>
+                        </li>
                         @endforeach
 
                         @guest
-                            @if (Route::has('login'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                                </li>
-                            @endif
+                        @if (Route::has('login'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        </li>
+                        @endif
 
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                         @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }}
+                        @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                        @endif
+                        @else
+                        <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                {{ Auth::user()->name }}
+                            </a>
+
+                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                    {{ __('Logout') }}
                                 </a>
 
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
+                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                    @csrf
+                                </form>
+                            </div>
+                        </li>
                         @endguest
                     </ul>
                 </div>
@@ -92,7 +98,8 @@
 
                         <div class="list-group list-small">
                             @foreach($categories as $category)
-                            <a href="{{ url($category->slug) }}" class="list-group-item d-flex justify-content-between align-items-center list-group-item-action">{{-- if active bg-muted --}}
+                            <a href="{{ url($category->slug) }}"
+                                class="list-group-item d-flex justify-content-between align-items-center list-group-item-action">{{-- if active bg-muted --}}
                                 {{ $category->name }}
                                 <span class="badge badge-primary badge-pill">{{ $category->posts->count() }}</span>
                             </a>
@@ -123,4 +130,5 @@
     <script src="{{ asset('js/app.js') }}"></script>
     @yield('afterjs')
 </body>
+
 </html>
